94b9f1482235d8d6e4d56787f19dfdd2a2a7082e
Thibault Lepez <tibolepezfr@gmail.com>
2018-11-21 15:52:01 +0100

    Correct the form
    and some other things

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -3,40 +3,48 @@
         <b-container>
             <h2>Find your destination</h2>
             <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-                <b-form-group id="exampleInputGroup1"
-                              label="Email address:"
-                              label-for="exampleInput1"
-                              description="We'll never share your email with anyone else.">
-                    <b-form-input id="exampleInput1"
-                                  type="email"
-                                  v-model="form.email"
-                                  required
-                                  placeholder="Enter email">
-                    </b-form-input>
-                </b-form-group>
-                <b-form-group id="exampleInputGroup2"
-                              label="Your Name:"
-                              label-for="exampleInput2">
-                    <b-form-input id="exampleInput2"
-                                  type="text"
-                                  v-model="form.name"
-                                  required
-                                  placeholder="Enter name">
-                    </b-form-input>
-                </b-form-group>
-                <b-form-group id="exampleInputGroup3"
-                              label="Food:"
-                              label-for="exampleInput3">
-                    <b-form-select id="exampleInput3"
-                                   :options="foods"
+                <b-form-group id="inputSectionGroupe"
+                              label="Votre section:"
+                              label-for="inputSection">
+                    <b-form-select id="inputSection"
+                                   :options="section"
                                    required
-                                   v-model="form.food">
+                                   v-model="form.section">
                     </b-form-select>
                 </b-form-group>
+                <b-form-group id="inputClassementGroup"
+                              label="Classement en L1"
+                              label-for="inputClassement">
+                    <b-form-input id="inputClassement"
+                                  type="integer"
+                                  v-model="form.classement"
+                                  required
+                                  placeholder="Votre classement en L1">
+                    </b-form-input>
+                </b-form-group>
+                <b-form-group id="inputGPAGroup"
+                              label="GPA du S3"
+                              label-for="inputGPAGroup">
+                    <b-form-input id="inputGPA"
+                                  type="integer"
+                                  v-model="form.gpa"
+                                  placeholder="GPA du S3">
+                    </b-form-input>
+                </b-form-group>
+                <b-form-group id="inputBudgetGroup"
+                              label="Budget"
+                              label-for="inputBudgetGroup">
+                    <b-form-input id="inputBudget"
+                                  type="integer"
+                                  v-model="form.budget"
+                                  placeholder="Votre budget">
+                    </b-form-input>
+                </b-form-group>
+
                 <b-form-group id="exampleGroup4">
                     <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-                        <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                        <b-form-checkbox value="that">Check that out</b-form-checkbox>
+                        <b-form-checkbox value="passport">Passport en cours de validité</b-form-checkbox>
+                        <b-form-checkbox value="bourse">Boursier</b-form-checkbox>
                     </b-form-checkbox-group>
                 </b-form-group>
                 <b-button type="submit" variant="primary">Submit</b-button>
@@ -54,15 +62,13 @@
         data () {
             return {
                 form: {
-                    email: '',
-                    name: '',
-                    food: null,
+                    section: null,
+                    classement: null,
+                    gpa: null,
+                    budget: null,
                     checked: []
                 },
-                foods: [
-                    { text: 'Select One', value: null },
-                    'Carrots', 'Beans', 'Tomatoes', 'Corn'
-                ],
+                section: ['International', 'Classique', 'Bio-numérique', 'Renforcé'],
                 show: true
             }
         },
@@ -75,9 +81,10 @@
             onReset (evt) {
                 evt.preventDefault();
                 /* Reset our form values */
-                this.form.email = '';
-                this.form.name = '';
-                this.form.food = null;
+                this.form.section = null;
+                this.form.classement = null;
+                this.form.gpa = null;
+                this.form.budget = null;
                 this.form.checked = [];
                 /* Trick to reset/clear native browser form validation state */
                 this.$nextTick(() => { this.show = true });
--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,13 +1,15 @@
 <template>
-    <div id="popupCard" class="container">
-        <div class="row align-items-center">
-            <div class="cols-6">
-                <img src="../img/no-image-2.png" alt="img description" style="height: 200px; width: 200px">
-            </div>
-            <div class="cols-6">
-                <h1>Titre</h1>
-                <p>text dans la carte</p>
-                <b-button @click="test">En savoir plus</b-button>
+    <div id="popupCard">
+        <div class="container" v-show="show">
+            <div class="row align-items-center">
+                <div class="cols-6">
+                    <img src="../img/no-image-2.png" alt="img description" style="height: 200px; width: 200px">
+                </div>
+                <div class="cols-6">
+                    <h1>Titre</h1>
+                    <p>text dans la carte</p>
+                    <b-button @click="test">En savoir plus</b-button>
+                </div>
             </div>
         </div>
         <slideout-panel></slideout-panel>
@@ -21,30 +23,22 @@
         name: "PopupCard",
         data () {
             return {
-                proprieties: Object
+                proprieties: Object,
+                show : true
             }
         },
 
         methods: {
 
             test() {
+                //this.show = false;
                 const panel1Handle = vueSlideoutPanelService.show({
                     component: 'slideout-panel',
-                    width: '500px', //customize the width
+                    width: '700px', //customize the width
                     keepAlive: true,
-                    props: {
-                        data: {
-                            firstName: 'John',
-                            lastName: 'Doe',
-                            age: 39
-                        }
-                    }
                 });
 
-                panel1Handle.promise
-                    .then(results => {
-                        console.log('Results for panel 1:', results);
-                    });
+
             }
         }
     }
