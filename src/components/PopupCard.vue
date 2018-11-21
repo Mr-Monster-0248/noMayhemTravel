bfd3765ae07113c47b123802ca3722116699d0d8
Thibault Lepez <tibolepezfr@gmail.com>
2018-11-21 12:42:01 +0100

    add panel

--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,24 +1,50 @@
 <template>
-    <div id="popupCard">
-        <b-card title="titre"
-                sub-title="Card subtitle">
-            <p class="card-text">
-                Some quick example text to build on the <em>card title</em> and make up the bulk of the card's content.
-            </p>
-            <a href="#"
-               class="card-link">Card link</a>
-            <b-link href="#"
-                    class="card-link">Another link</b-link>
-        </b-card>
+    <div id="popupCard" class="container">
+        <div class="row align-items-center">
+            <div class="cols-6">
+                <img src="../img/no-image-2.png" alt="img description" style="height: 200px; width: 200px">
+            </div>
+            <div class="cols-6">
+                <h1>Titre</h1>
+                <p>text dans la carte</p>
+                <b-button @click="test">En savoir plus</b-button>
+            </div>
+        </div>
+        <slideout-panel></slideout-panel>
     </div>
 </template>
 
 <script>
+    import { vueSlideoutPanelService } from 'vue2-slideout-panel';
+
     export default {
         name: "PopupCard",
         data () {
             return {
                 proprieties: Object
+            }
+        },
+
+        methods: {
+
+            test() {
+                const panel1Handle = vueSlideoutPanelService.show({
+                    component: 'slideout-panel',
+                    width: '500px', //customize the width
+                    keepAlive: true,
+                    props: {
+                        data: {
+                            firstName: 'John',
+                            lastName: 'Doe',
+                            age: 39
+                        }
+                    }
+                });
+
+                panel1Handle.promise
+                    .then(results => {
+                        console.log('Results for panel 1:', results);
+                    });
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import bootstrapVue from "bootstrap-vue";
+import { VueSlideoutPanel } from 'vue2-slideout-panel';
 
 Vue.config.productionTip = false;
 Vue.use(bootstrapVue);
+Vue.component('slideout-panel', VueSlideoutPanel);
 
 new Vue({
   render: h => h(App)
