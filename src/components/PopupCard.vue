8437efd569c935d36394208fa209eeebd5b03873
Gabriel Dugny <gabriel.dugny@gmail.com>
2018-12-01 20:01:43 +0100

    Allowed further customization of Popup
    Cleared mapbox default popup,
    still need to fix close button.

--- src/components/PopupCard.vue
+++ src/components/PopupCard.vue
@@ -1,16 +1,24 @@
 <template>
     <div id="popupCard">
         <div class="container">
-            <div class="row align-items-center">
-                <div class="cols-6">
-                    <img :src="this.destination.thumbnail" :alt="this.destination.name">
+            <b-card bg-variant="warning"
+                    :header="'<h1 class=\'popuph1\'>'+this.destination.country+'</h1>'"
+                    text-variant="white"
+                    class="text-center">
+
+                <div class="row align-items-center">
+                    <div class="cols-6">
+                        <b-img :src="this.destination.thumbnail" :alt="this.destination.name" width="200" height="200"
+                               rounded fluid/>
+                    </div>
+                    <div class="cols-6 popuptext">
+
+                        <h3>{{ this.destination.name }}</h3>
+                        <b-button class="d-flex align-items-end" @click="launchPanel">En savoir plus</b-button>
+                    </div>
                 </div>
-                <div class="cols-6">
-                    <h1>{{ this.destination.country }}</h1>
-                    <h3>{{ this.destination.name }}</h3>
-                    <b-button @click="launchPanel">En savoir plus</b-button>
-                </div>
-            </div>
+            </b-card>
+
         </div>
 
     </div>
@@ -42,7 +50,6 @@
 
         methods: {
             launchPanel() {
-
                 console.log("WTF");
                 EventBus.$emit('clickPanel', 1);
             }
@@ -53,6 +60,31 @@
     }
 </script>
 
-<style scoped>
+<style>
+    .container {
+        padding-right: 0px;
+        padding-left: 0px;
+    }
 
+    .popuptext {
+        margin-left: 10px;
+    }
+
+    .card-header h1 {
+        margin-bottom: 0rem;
+    }
+
+    .card-header {
+        padding: 0rem;
+        margin-bottom: 0;
+    }
+
+    .mapboxgl-popup-content {
+        position: relative;
+        background: transparent;
+        border-radius: 0px;
+        box-shadow: none;
+        padding: 0px 0px 0px;
+        pointer-events: auto;
+    }
 </style>
