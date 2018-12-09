8d23f0314ebd2e6f838dd8a6a0a3ccbab49f5e65
Thibault Lepez <tibolepezfr@gmail.com>
2018-12-09 17:31:40 +0100

    Info style update

--- src/components/MainInfo.vue
+++ src/components/MainInfo.vue
@@ -3,14 +3,29 @@
         <h2>What is Immersion Time?</h2>
         <p>It's a tool to quickly visualize and gather information about all destinations for your L3 at Efrei.
         It features a matching algorithm that could suggest you some destinations you might not have thought a lot about...</p>
-        <h3>Team</h3>
-        <p>The team was composed of LEPEZ Thibault, LEMDANI Aïda, BOUREY Louise, DUGNY Gabriel and LACROUTS Vincent; P2022, L2 INT</p>
+        <h3>L'Equipe de Mayhem Adventure</h3>
+        <b-table striped hover :items="items"></b-table>
     </div>
 </template>
 
 <script>
+    const items = [
+        {Nom: 'Gabriel DUGNY', post: 'Chef de projet'},
+        {Nom: 'Thibault Lepez', post: 'Responsable technique'},
+        {Nom: 'Aïda LEMDANI', post: 'Responsable design'},
+        {Nom: 'Louise BOUREY', post: 'Responsable communication'},
+        {Nom: 'Vincent LACROUTS', post: 'Testeur agile'}
+
+    ];
+
     export default {
-        name: "MainInfo"
+        data () {
+            return {
+                name: "MainInfo",
+
+                items: items
+            }
+        }
     }
 </script>
 
