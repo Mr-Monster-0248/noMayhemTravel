00c5a410296d63a0527ade5fb105449288396c19
Aïda LEMDANI <44293550+TheIBAM@users.noreply.github.com>
2018-12-13 19:30:41 +0100

    Update MainInfo.vue

--- src/components/MainInfo.vue
+++ src/components/MainInfo.vue
@@ -3,10 +3,10 @@
         <h2>Qu'est ce qu'Immersion Time?</h2>
        <p>Dans le cadre du projet de programmation de L2, Mayhem Adventure présente un site dédié exclusivement aux étudiants d’Efrei Paris.</p> 
        <p>Il permet, à titre théorique, d’étudier vos possibilités de mobilités à l’international.
-          Notre équipe a pour ambition de te faire découvrir des destinations auxquelles tu n’avais peut-être pas pensé.</p>
+          Notre équipe a pour ambition de te faire découvrir des destinations auxquelles tu n’avais peut-être pas pensé.
            </p>
-       <p>Cependant, ce site ne peut en aucun cas vous assurer l’application des destinations obtenues. </p>
-        <p>Ces dernières évoluant en fonction de paramètres étant propres à chaque promotions d’Efrei Paris.</p>
+          Cependant, ce site ne peut en aucun cas vous assurer l’application des destinations obtenues.
+          Ces dernières évoluant en fonction de paramètres étant propres à chaque promotions d’Efrei Paris.
            </p>
         <h3>L'Équipe de Mayhem Adventure</h3>
         <b-table striped hover :items="items"></b-table>
