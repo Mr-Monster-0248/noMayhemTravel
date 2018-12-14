2197cb5073814c586f37e4c3836396169bfa8585
Gabriel Dugny <gabriel.dugny@gmail.com>
2018-12-14 18:53:47 +0100

    Rollback changes to main

--- src/components/MainInfo.vue
+++ src/components/MainInfo.vue
@@ -11,23 +11,17 @@
             Ces dernières évoluant en fonction de paramètres étant propres à chaque promotions d’Efrei Paris.
         </p>
         <h3>L'Équipe de Mayhem Adventure</h3>
-        <b-table striped hover :items="items"></b-table>
-=======
-        Il permet, à titre théorique, d’étudier vos possibilités de mobilités à l’international. Notre équipe a pour ambition de te faire découvrir des destinations auxquelles tu n’avais peut-être pas pensé
-        Cependant, ce site ne peut en aucun cas vous assurer l’application des destinations obtenues. Ces dernières évoluant en fonction de paramètres étant propres à chaque promotions d’Efrei Paris.</p>
-        <h3>Team</h3>
-        <p>The team was composed of LEPEZ Thibault, LEMDANI Aïda, BOUREY Louise, DUGNY Gabriel and LACROUTS Vincent; P2022, L2 INT</p>
->>>>>>> Stashed changes
+        <b-table striped hover small :items="items"></b-table>
     </div>
 </template>
 
 <script>
     const items = [
-        {Nom: 'Gabriel DUGNY', post: 'Chef de projet'},
-        {Nom: 'Thibault LEPEZ', post: 'Responsable technique'},
-        {Nom: 'Aïda LEMDANI', post: 'Responsable design'},
-        {Nom: 'Louise BOUREY', post: 'Responsable communication'},
-        {Nom: 'Vincent LACROUTS', post: 'Testeur agile'}
+        {Nom: 'Gabriel DUGNY', poste: 'Chef de projet'},
+        {Nom: 'Thibault LEPEZ', poste: 'Responsable technique'},
+        {Nom: 'Aïda LEMDANI', poste: 'Responsable design'},
+        {Nom: 'Louise BOUREY', poste: 'Responsable communication'},
+        {Nom: 'Vincent LACROUTS', poste: 'Testeur agile'}
 
     ];
 
